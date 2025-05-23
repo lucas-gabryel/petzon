6a9edadb797f92b57aca13278d5cdfb5ffc23535
Lucas Gabryel <lucas-gabryel@outlook.com>
2025-05-23 10:49:34 -0300

    iniciando na gallery

--- app/[locale]/gallery/page.tsx
+++ app/[locale]/gallery/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useTranslations } from "next-intl";
+import Typography from "@mui/material/Typography";
 
 export default function Gallery() {
-  return <h1>Gallery</h1>;
+  const t = useTranslations("Gallery");
+
+  return (
+    <>
+      <Typography variant="h1">{t("title")}</Typography>
+      <Typography variant="h2">{t("about")}</Typography>
+    </>
+  );
 }
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useTranslations } from 'next-intl';
+import Gallery from "./gallery/page";
 
 export default function Home() {
   const t = useTranslations("Home");
 
   return (
     <>
-      <h1>{t("title")}</h1>
-      <p>{t("about")}</p>
+      {/* <h1>{t("title")}</h1>
+      <p>{t("about")}</p> */}
+      <Gallery />
     </>
   );
 }
